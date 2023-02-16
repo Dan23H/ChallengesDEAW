fe13ee07e1ee4c9d2b67c946623572f7ed23aafa
HexDan <123597766+Dan23H@users.noreply.github.com>
2023-02-16 22:20:38 +0000

    modified:   project_deaw/src/components/GifGrid.jsx

--- project_deaw/src/components/GifGrid.jsx
+++ project_deaw/src/components/GifGrid.jsx
@@ -1,9 +1,24 @@
-export const GifGrid = ({category}) => {
-    console.log(category)
+const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=KYxmtA1qz27qrW5rSjakDwV8FCRLJHf2&q=${category}&limit=25&offset=0&rating=g&lang=en`;
+    const resp = await fetch(url)
+    const { data } = await resp.json()
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url
+        }
+    })
+
+    return gifs
+}
+
+export const GifGrid = ({ category }) => {
+    getGifs(category)
     return (
         <>
             <h3>{ category }</h3>
-            <p>Hello World!</p>
+            <p> Hello World! </p>
         </>
     )
 }
