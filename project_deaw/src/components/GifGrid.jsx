1f9ccb8cbb58e04447287e8d7e43e51194307d13
HexDan <123597766+Dan23H@users.noreply.github.com>
2023-02-23 21:00:04 +0000

    modified:   project_deaw/src/components/GifGrid.jsx

--- project_deaw/src/components/GifGrid.jsx
+++ project_deaw/src/components/GifGrid.jsx
@@ -1,13 +1,25 @@
 import { useEffect, useState } from "react"
 const getGifs = async (category) => {
-    const url = ``
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=KYxmtA1qz27qrW5rSjakDwV8FCRLJHf2&q=${category}&limit=25&offset=0&rating=g&lang=en`
+    const resp = await fetch(url)
+    const {data} = await resp.json()
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url
+        }
+    })
+    return gifs
 }
 
 export const GifGrid = ({ category }) => {
-    const [images,getImages] = useState([])
+    const [images,setImages] = useState([])
     getGifs = async() => {
-        console.log(images)
+        const images = await getGifs(category)
+        setImages(images)
     }
+
     useEffect(() => {
         getGifs() // eslint-disable-next-line
     }, [])
@@ -15,7 +27,17 @@ export const GifGrid = ({ category }) => {
     return (
         <>
             <h3>{ category }</h3>
-            <p>Hello World!</p>
+            <div className="GifsCSS">
+                {
+                    images.map((image,key) => {
+                        return GifItem(image,key)
+                    })
+                }
+            </div>
         </>
     )
+}
+
+const GifItem = (image,key) => {
+    return 
 }
