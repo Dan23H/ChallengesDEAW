754582fec6a006354abf494d420f1bfda833c2fa
HexDan <123597766+Dan23H@users.noreply.github.com>
2023-02-23 21:45:13 +0000

    modified:   project_deaw/src/components/GifGrid.jsx 	new file:   project_deaw/src/helpers/GetGifs.jsx 	new file:   project_deaw/src/hooks/useFetchGifs.jsx

--- project_deaw/src/components/GifGrid.jsx
+++ project_deaw/src/components/GifGrid.jsx
@@ -1,39 +1,16 @@
-import { useEffect, useState } from "react"
 import { GifItem } from "./GifItem"
-const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=KYxmtA1qz27qrW5rSjakDwV8FCRLJHf2&q=${category}&limit=25&offset=0&rating=g&lang=en`
-    const resp = await fetch(url)
-    const {data} = await resp.json()
-    const gifs = data.map( img => {
-        return {
-            id: img.id,
-            title: img.title,
-            url: img.images.downsized_medium.url
-        }
-    })
-    return gifs
-}
+import { useFetchGifs } from "../hooks/useFetchGifs"
 
 export const GifGrid = ({ category }) => {
-    const [images,setImages] = useState([]) 
+    const {images,isLoading} = useFetchGifs( category ) 
     
-    // eslint-disable-next-line
-    const getImages = async() => { 
-        const images = await getGifs(category)
-        setImages(images)
-    }
-
-    useEffect(() => {
-        getImages() // eslint-disable-next-line
-    }, [])
-
     return (
         <>
             <h3>{ category }</h3>
             <div className="GifsCSS">
                 {
                     images.map((image,key) => {
-                        return <GifItem key={key} {...image} />
+                        return <GifItem key={key} {...image} ></GifItem>
                     })
                 }
             </div>
---
+++ project_deaw/src/hooks/useFetchGifs.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/GetGifs"
+
+export const useFetchGifs = (category) => {
+    const [gifsList, setGifsList] = useState([]);
+
+    const newFunction = async () => {
+        const gifsList = await getGifs(category)
+        setGifsList(gifsList)
+    }
+
+
+    useEffect(() => {
+        newFunction(); // eslint-disable-next-line
+    }, [])
+    return {
+        images: gifsList,
+        isLoading: false
+    }
+}
