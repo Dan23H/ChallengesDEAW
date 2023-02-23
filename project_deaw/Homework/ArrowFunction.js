8539a70e13a95b0c0c0a8a78889f24695749248c
HexDan <123597766+Dan23H@users.noreply.github.com>
2023-02-23 17:54:18 +0000

    new file:   project_deaw/Homework/ArrowFunction.js 	new file:   project_deaw/Homework/RegularFunction.js

---
+++ project_deaw/Homework/ArrowFunction.js
@@ -0,0 +1,7 @@
+const isOdd = (number) => {
+    if (number % 2 === 0) {
+        return "Even."
+    } else {
+        return "Odd."
+    }
+}
---
+++ project_deaw/Homework/RegularFunction.js
@@ -0,0 +1,16 @@
+// New regular function
+const isOdd_New = function (number) {
+    if (number % 2 === 0) {
+        return "Even."
+    } else {
+        return "Odd."
+    }
+}
+// Old regular function
+function isOdd_Old (number) {
+    if (number % 2 === 0) {
+        return "Even."
+    } else {
+        return "Odd."
+    }
+}
