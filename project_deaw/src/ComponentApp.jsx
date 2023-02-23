5cf38a9c14daf3d0bb8f6a184051b0b7e3108a7e
HexDan <123597766+Dan23H@users.noreply.github.com>
2023-02-23 19:10:33 +0000

    modified:   project_deaw/src/ComponentApp.jsx

--- project_deaw/src/ComponentApp.jsx
+++ project_deaw/src/ComponentApp.jsx
@@ -3,19 +3,19 @@ export const ComponentApp = () => {
     // Challenge3:
     const [categories,setCategories] = useState([])
     const [category, setCategory] = useState('')
-    const onAddCategory = () => {
+    const addingCategories = () => {
         setCategories(array => [...array, category])
         setCategory('')
     }
-    const onSetCategory = (evt) => {
+    const settingCategory = (evt) => {
         setCategory(evt.target.value)
 
     }
     return(
         <>
             <h1>GifExpert</h1>
-            <input type="text" value={category} onChange={(event) => {onSetCategory(event)}} />
-            <button onClick={() => onAddCategory()}> Add Category </button>
+            <input type="text" value={category} onChange={(event) => {settingCategory(event)}} />
+            <button onClick={() => addingCategories()}> Add Category </button>
             <ol>
                 {
                     categories.map(
