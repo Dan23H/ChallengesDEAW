0f37bc5d9d3b8b8d169e00dd882fe3c5706e6524
HexDan <123597766+Dan23H@users.noreply.github.com>
2023-02-23 21:26:25 +0000

    modified:   project_deaw/src/components/GifGrid.jsx

--- project_deaw/src/components/GifGrid.jsx
+++ project_deaw/src/components/GifGrid.jsx
@@ -40,3 +40,23 @@ export const GifGrid = ({ category }) => {
         </>
     )
 }
+
+export const useFetchGifs = (category) => {
+    const [gifsList, setGifsList] = useState([]);
+
+    const newFunction = async () => {
+        const gifsList = await getGifs(category)
+        setGifsList(gifsList)
+    }
+
+
+    useEffect(() => {
+        newFunction(); // eslint-disable-next-line
+    }, [])
+    return {
+        images: gifsList,
+        isLoading: false,
+        error: null
+    }
+}
+
