7109ee8cf51a41181950af65e493df4a1ade647b
HexDan <123597766+Dan23H@users.noreply.github.com>
2023-02-23 22:06:22 +0000

    renamed:    project_deaw/src/components/GetGifs.js -> project_deaw/src/helpers/GetGifs.jsx 	modified:   project_deaw/src/hooks/useFetchGifs.jsx 	modified:   project_deaw/src/index.js

--- project_deaw/src/index.js
+++ project_deaw/src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Counter } from './Counter';
-//import Function from './Functions';
-import {ComponentApp} from './ComponentApp.jsx'
-
-import { GifExpertApp } from './GifExpertApp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
