60ddd0027439058aa802f4ae5b29fca7732e5204
Dan23H <123597766+Dan23H@users.noreply.github.com>
2023-04-25 18:03:02 -0500

    modified:   backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 require('dotenv').config()
+const {dbConnection} = require('./database/config')
+
 const bodyParser = require('body-parser')
 
 // Crear Express App
 const app = express();
 
+// Base de datos
+dbConnection();
+
 app.use(express.static('public'))
+
+// Lectura y parseo del body
 app.use(bodyParser.json())
 
 // Rutas
