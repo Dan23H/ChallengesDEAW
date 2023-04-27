8f8ca211c8892ffb5af44edd634589120dc770c0
HexDan <123597766+Dan23H@users.noreply.github.com>
2023-04-27 17:46:29 -0500

    modified:   backend/.env 	modified:   backend/helpers/jwt.js

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken')
 
 const generarJWT = (uid, name) => {
-    return new Promise((resolve,reject) => {
-        const payload = {uid:uid,name:name}
-        jwt.sign(payload,process.env.SECRET_JWT_SEED,{
+    return new Promise((resolve, reject) => {
+        const payload = { uid: uid, name: name }
+        jwt.sign(payload, process.env.SECRET_JWT_SEED, {
             expiresIn: '2h',
         }, (error, token) => {
-            if (error){
+            if (error) {
                 console.log(error)
                 reject('No se pudo generar el token.')
             }
@@ -15,4 +15,4 @@ const generarJWT = (uid, name) => {
     })
 }
 
-module.exports = {generarJWT}
+module.exports = { generarJWT }
