ec5f9121a734a42166b1d65e6446ef46c6603394
HexDan <daniel.hernandez_val@uao.edu.co>
2023-05-03 16:54:18 -0500

    Falta info a partir de la diapositiva Joining Schemas

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -1,3 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const { validarJWT } = require('../middlewares/validar-token')
+const { listarTasks, crearTask, actualizarTask, eliminarTask } = require('../controllers/task.js')
+
+router.use(validarJWT)
+
+router.get('/', listarTasks )
+router.post('/', crearTask)
+router.put('/:id', actualizarTask)
+router.delete('/:id', eliminarTask)
+
+module.exports = router
